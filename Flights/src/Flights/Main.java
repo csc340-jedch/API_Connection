b59e4a5d044758c9bfdb4753cbbc71ebb9805382
Gene Pressinger <genepressinger@yahoo.com>
2021-03-30 22:27:43 -0400

    Fixed several exception errors
    Some loops wouldn't even start, since some flight arrays only had length of 1, and the loop started after the first index. Also, some loops stopped AFTER it went through the entire array.

--- Flights/src/Flights/Main.java
+++ Flights/src/Flights/Main.java
@@ -7,13 +7,10 @@ public class Main {
     public static void main(String[] args) throws JSONException, NotDirectFlightException {
 
 	GetFlightData test = new GetFlightData();
-        System.out.println("Carrier " + test.getCarrier("2021-04-01","ORD"));
+        System.out.println("Carrier: " + test.getCarrier("2021-04-01","ORD"));
         System.out.println("Which quote is direct? " + test.isDirectFlight("2021-04-01","ORD"));
-        System.out.println("Price " + test.getPrice("2021-04-01","ORD"));
-        System.out.println("Carrier ID " + test.getCarrierId("2021-04-01","ORD"));
+        System.out.println("Price: " + test.getPrice("2021-04-01","ORD"));
+        System.out.println("Carrier ID: " + test.getCarrierId("2021-04-01","ORD"));
         System.out.println("Is Direct Flight: " + test.isDirectFlight("2021-04-01","ORD"));
-
-
-	//test.getQuote("2019-09-01","SDF");
     }
 }
